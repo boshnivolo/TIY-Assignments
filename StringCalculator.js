a9c4b8bafb914e199cc3eaf2474ad5d624b41a3c
Jasmine Baik <boshnivolo@gmail.com>
2015-09-03 00:31:01 -0400

    still can figure it out...

--- StringCalculator.js
+++ StringCalculator.js
@@ -5,31 +5,50 @@ test('fromEnglish: "zero" through "nine"'), function(){
   expect(fromEnglish).to.exist;
   expect(fromEnglish("zero")).to.equal(0);
   expect(fromEnglish("one")).to.equal(1);
+  expect(fromEnglish("two")).to.equal(2);
+  expect(fromEnglish("three")).to.equal(3);
+  expect(fromEnglish("four")).to.equal(4);
+  expect(fromEnglish("five")).to.equal(5);
+  expect(fromEnglish("six")).to.equal(6);
+  expect(fromEnglish("seven")).to.equal(7);
+  expect(fromEnglish("eight")).to.equal(8);
+  expect(fromEnglish("nine")).to.equal(9);
   // more tests here, please...
 }); // END test(fromEnglish)
 
 /**
- * @param {String} word representing an English number
- * @return {Number} value corresponding to `word`
+ * @param {"zero"} word representing an English number
+ * @return {1} value corresponding to `word`
  */
 function fromEnglish(word){
-  // fill this in after writing tests...
+  return word.toNumber()
 } // END fromEnglish
 
 test('plus: ("zero", "zero") through ("zero", "nine")', function(){
   expect(plus).to.exist;
   expect(plus("zero", "zero")).to.equal(0);
   expect(plus("zero", "one")).to.equal(1); // change just one thing!
-  expect(plus("zero", "two")).to.equal(2); // ha!
+  expect(plus("zero", "two")).to.equal(2);
+  expect(plus("zero", "three")).to.equal(3);
+  expect(plus("zero", "four")).to.equal(4);
+  expect(plus("zero", "five")).to.equal(5);
+  expect(plus("zero", "six")).to.equal(6);
+  expect(plus("zero", "seven")).to.equal(7);
+  expect(plus("zero", "eight")).to.equal(8);
+  expect(plus("zero", "nine")).to.equal(9); // ha!
   // write more tests like those...
 }); // END test(plus)
 
+
+function plus(A, B){
+  return A + B
+}
 /**
- * @param {String} A representing an English number word
+ * @param {String} var A ==
  * @param {String} B representing an English number word
  * @return {Number} the value of `A` added to `B`
 function plus(A, B){
-  // write those tests first, please...
+  return A + B
 } // END plus
 
 test('minus: ("zero", "zero") through ("zero", "nine")', function(){
