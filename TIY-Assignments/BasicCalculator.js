58b354bb43a35337feb794f95fb5e799d91c3346
Jasmine Baik <boshnivolo@gmail.com>
2015-09-08 10:39:12 -0400

    tea

---
+++ TIY-Assignments/BasicCalculator.js
@@ -0,0 +1,64 @@
+console.assert(add(0, 0),===0);
+console.assert(add(0, 1),===1);
+console.assert(add(0, 2),===2);
+console.assert(add(0, 3),===3);
+console.assert(add(0, 4),===4);
+console.assert(add(0, 5),===5);
+console.assert(add(0, 6),===6);
+console.assert(add(0, 7),===7);
+console.assert(add(0, 8),===8);
+console.assert(add(0, 9),===9);
+console.assert(add(1, 1),===2);
+console.assert(add(1, 2),===3);
+console.assert(add(1, 3),===4);
+console.assert(add(1, 4),===5);
+console.assert(add(1, 5),===6);
+console.assert(add(1, 6),===7);
+console.assert(add(1, 7),===8);
+console.assert(add(1, 8),===9);
+console.assert(add(1, 9),===10);
+console.assert(add(2, 2),===4);
+console.assert(add(2, 3),===5);
+console.assert(add(2, 4),===6);
+console.assert(add(2, 5),===7);
+console.assert(add(2, 6),===8);
+console.assert(add(2, 7),===9);
+console.assert(add(2, 8),===9);
+console.assert(add(2, 9),===11);
+console.assert(add(3, 3),===6);
+console.assert(add(3, 4),===7);
+console.assert(add(3, 5),===8);
+console.assert(add(3, 6),===9);
+console.assert(add(3, 7),===10);
+console.assert(add(3, 8),===11);
+console.assert(add(3, 9),===12);
+console.assert(add(4, 4),===8);
+console.assert(add(4, 5),===9);
+console.assert(add(4, 6),===10);
+console.assert(add(4, 7),===11);
+console.assert(add(4, 8),===12);
+console.assert(add(4, 9),===13);
+console.assert(add(5, 5),===10);
+console.assert(add(5, 6),===11);
+console.assert(add(5, 7),===12);
+console.assert(add(5, 8),===13);
+console.assert(add(5, 9),===14);
+console.assert(add(6, 6),===12);
+console.assert(add(6, 7),===13);
+console.assert(add(6, 8),===14);
+console.assert(add(6, 9),===15);
+console.assert(add(7, 7),===14);
+console.assert(add(7, 8),===15);
+console.assert(add(7, 9),===16);
+console.assert(add(8, 8),===16);
+console.assert(add(8, 9),===17);
+console.assert(add(9, 9),===18);
+
+
+/**
+ * @param {Number} A to add to B
+ * @param {Number} B to add to A
+ * @return {Number} sum of A and B
+ */
+function add(A, B){
+} // END add
