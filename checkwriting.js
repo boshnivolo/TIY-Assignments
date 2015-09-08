2963fb8b06beb1d6fb02d549f43c1bd02ec1f5d5
Jasmine Baik <boshnivolo@gmail.com>
2015-09-08 10:37:58 -0400

    clearing

--- checkwriting.js
+++ checkwriting.js
@@ -1,5 +1,11 @@
 var test = require('mocha').it,
   assert = require('chai').assert;
+var expect = require('chai').expect;
+
+it('should convert numbers into string', function(){
+  expect(true).to.be.true;
+  expect(toEnglish).to.be.a('function');
+});
 
 test('toEnglish: `0` to `9`', function(){
   assert.isDefined(toEnglish);
@@ -14,44 +20,143 @@ test('toEnglish: `0` to `9`', function(){
   assert.equal(toEnglish(7), "seven");
   assert.equal(toEnglish(8), "eight");
   assert.equal(toEnglish(9), "nine");
+  expect(fromEnglish("zero")).to.equal(0);
 }); // END test(toEnglish)
-function fromEnglish(word){
-  if (word === "one"){
-    return 1;
+function toEnglish(number){
+  if (number === 0){
+    return "zero";
   }
-  if (word === "two"){
-    return 2;
+  if (number === 1){
+    return "one";
   }
-  if (word === "three"){
-    return 3;
+  if (number === 2){
+    return "two";
   }
-  if (word === "four"){
-    return 4;
+  if (number === 3){
+    return "three";
   }
-  if (word === "five"){
-    return 5;
+  if (number === 4){
+    return "four";
   }
-  if (word === "six"){
-    return 6;
+  if (number === 5){
+    return "five";
   }
-  if (word === "seven"){
-    return 7;
+  if (number === 6){
+    return "six";
   }
-  if (word === "eight"){
-    return 8;
+  if (number === 7){
+    return "seven";
   }
-  if (word === "nine"){
-    return 9;
+  if (number === 8){
+    return "eight";
   }
-
-  return 0;
-}
+  if (number === 9){
+    return "nine";
+  }
+};
 
 test('toEnglish: `10` to `20`', function(){
+  assert.isDefined(toEnglish);
+  assert.isFunction(toEnglish);
+  assert.equal(toEnglish(10), "ten");
+  assert.equal(toEnglish(11), "eleven");
+  assert.equal(toEnglish(12), "twelve");
+  assert.equal(toEnglish(13), "thirteen");
+  assert.equal(toEnglish(14), "fourteen");
+  assert.equal(toEnglish(15), "fifteen");
+  assert.equal(toEnglish(16), "sixteen");
+  assert.equal(toEnglish(17), "seventeen");
+  assert.equal(toEnglish(18), "eighteen");
+  assert.equal(toEnglish(19), "nineteen");
+});
+
+function toEnglish(number){
+  if (number === 10){
+    return "ten";
+  }
+  if (number === 11){
+    return "eleven";
+  }
+  if (number === 12){
+    return "twelve";
+  }
+  if (number === 13){
+    return "thirteen";
+  }
+  if (number === 14){
+    return "fourteen";
+  }
+  if (number === 15){
+    return "fifteen";
+  }
+  if (number === 16){
+    return "sixteen";
+  }
+  if (number === 17){
+    return "seventeen";
+  }
+  if (number === 18){
+    return "eighteen";
+  }
+  if (number === 19){
+    return "nineteen";
+  }
+}
+
   // use `assert` again...
 }); // END test(toEnglish)
 
 test('toEnglish: `21` to `30`', function(){
+  test('toEnglish: `0` to `9`', function(){
+    assert.isDefined(toEnglish);
+    assert.isFunction(toEnglish);
+    assert.equal(toEnglish(20), "twenty");
+    assert.equal(toEnglish(21), "twenty-one");
+    assert.equal(toEnglish(22), "twenty-two");
+    assert.equal(toEnglish(23), "twenty-three");
+    assert.equal(toEnglish(24), "twenty-four");
+    assert.equal(toEnglish(25), "twenty-five");
+    assert.equal(toEnglish(26), "twenty-six");
+    assert.equal(toEnglish(27), "twenty-seven");
+    assert.equal(toEnglish(28), "twenty-eight");
+    assert.equal(toEnglish(29), "twenty-nine");
+    assert.equal(toEnglish(30), "thirty");
+  }); // END test(toEnglish)
+  function toEnglish(number){
+    if (number === 20){
+      return "twenty";
+    }
+    if (number === 21){
+      return "twenty-one";
+    }
+    if (number === 22){
+      return "twenty-two";
+    }
+    if (number === 23){
+      return "twenty-three";
+    }
+    if (number === 24){
+      return "twenty-four";
+    }
+    if (number === 25){
+      return "twenty-five";
+    }
+    if (number === 26){
+      return "twenty-six";
+    }
+    if (number === 27){
+      return "twenty-seven";
+    }
+    if (number === 28){
+      return "twenty-eight";
+    }
+    if (number === 29){
+      return "twenty-nine";
+    }
+    if (number === 30){
+      return "thirty";
+    }
+  };
   // this is where `toEnglish` starts to get interesting...
 }); // END test(toEnglish)
 
