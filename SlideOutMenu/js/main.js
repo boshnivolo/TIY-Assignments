99fe8824f910620c29e1507f5a463022336177bb
Jasmine Baik <boshnivolo@gmail.com>
2015-10-11 16:17:15 -0400

    Toggles class of

--- SlideOutMenu/js/main.js
+++ SlideOutMenu/js/main.js
@@ -1,24 +1,24 @@
 $(' a[href="#spirits"]').on('click', function (){
   console.log('tracer 1');
-  $(' section#spirits').toggleClass('active');
+  $(' section#spirits').toggleClass('active').siblings().removeClass('active');
 });
 
 $(' a[href="#beverages"]').on('click', function (){
   console.log('tracer 2');
-  $(' section#beverages').toggleClass('active');
+  $(' section#beverages').toggleClass('active').siblings().removeClass('active');
 });
 
 $(' a[href="#liquors"]').on('click', function (){
   console.log('tracer 3');
-  $(' section#liquors').toggleClass('active');
+  $(' section#liquors').toggleClass('active').siblings().removeClass('active');
 });
 
 $(' a[href="#mission"]').on('click', function (){
   console.log('tracer 4');
-  $(' section#mission').toggleClass('active');
+  $(' section#mission').toggleClass('active').siblings().removeClass('active');
 });
 
 $(' a[href="#contact"]').on('click', function (){
   console.log('tracer 5');
-  $(' section#contact').toggleClass('active');
+  $(' section#contact').toggleClass('active').siblings().removeClass('active');
 });
